c436ca95f713e97ef23f9d625f46c9e02fe9750c
Emil Georgi <emil376g@edu.campusvejle.dk>
2018-12-06 11:21:29 +0100

    fixed error on reload #3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { NotePostDetailComponent } from './note-post-detail/note-post-detail.com
 import { NoteComponent } from './note/note.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/index', pathMatch: 'full'},
   {path: 'index', component: IndexComponent},
   {path: 'note', component: NoteComponent},
   {path: 'detail/:id', component: NotePostDetailComponent}
